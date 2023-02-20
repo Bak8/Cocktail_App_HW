91111b164f9c10a5011a883de5d8c0698c2c23f5
Bak8 <ataibegaliev@gmail.com>
2023-02-20 20:52:41 +0600

    Initial Commit

---
+++ Cocktail_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktail_App
+//
+//  Created by Atai Begaliev on 20/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
